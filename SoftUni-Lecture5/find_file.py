0efb75b8318005ea19a6e2d7c6cc1a945f79e040
natla <natalia.malkovska@gmail.com>
2016-07-23 11:10:08 +0300

    Expanded solution of find_file.py

--- SoftUni-Lecture5/find_file.py
+++ SoftUni-Lecture5/find_file.py
@@ -1,21 +1,36 @@
-# Задача: 1. Търсене във файловата система
+"""
+Задача: 1. Търсене във файловата система
+Допълнение на задачата:
+Разширете вашия код, така че да може да търсите с прост wild card * - в началото или в края на името.
+"""
 
 import os
 import sys
+from fnmatch import fnmatch
 
 if len(sys.argv) < 3:
-    print("You need to provide directory and filename in the terminal: python find-file.py <dir> <filename>")
+    print("You need to provide directory and at least part of the filename in the terminal:\n"
+          "python find_file.py <dir> <filename> or python find_file.py <dir> <*file_part*>")
 elif not os.path.exists(sys.argv[1]):
     print("Directory doesn't exist")
 else:
     search_dir = sys.argv[1]
     search_file = sys.argv[2]
+
     found = []
     for root, dirs, files in os.walk(search_dir):
-        if os.path.isfile(os.path.join(root, search_file)):
-            result = os.path.dirname(os.path.join(root, search_file))
-            found.append(result)  # if the file is found more than once the results get appended to a list
-    if len(found) == 0:
-        print("File not found")
+        path = os.path.join(root, search_file)
+        if os.path.isfile(path):
+            result = os.path.dirname(path)
+            found.append(result)
+        else:
+            for file in files:
+                if fnmatch(file, search_file):
+                    result = root
+                    if result not in found:
+                        found.append(result)
+
+    if len(found) > 0:
+        print("These directories contain file(s) {}:\n{}".format(search_file, '\n'.join(found)))
     else:
-        print('\n'.join(found))
+        print("File not found")
