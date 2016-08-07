d7aaff268d41f5d4575ef27b3103284ea2727dc6
natla <natalia.malkovska@gmail.com>
2016-08-07 18:03:51 +0300

    OOP problem + solution

---
+++ SoftUni-L8-OOP/figures/draw_functions.py
@@ -0,0 +1,54 @@
+import sys
+import json
+import turtle
+
+from figures.simple_figures import Circle, Square, Rectangle, Pie, RegularPolygon
+
+
+def main():
+    try:
+        input_data = load_input_data("figures_simple.json")
+        figures = create_figures(input_data)
+        draw_figures(figures)
+    except Exception as e:
+        print("Invalid input file provided! Error:", e)
+
+
+def load_input_data(input_filename):
+    with open(input_filename) as f:
+        input_data = json.load(f)
+        return input_data
+
+
+FIGURE_TYPES = {
+    'square': Square,
+    'circle': Circle,
+    'rectangle': Rectangle,
+    'pie': Pie,
+    'reg_polygon': RegularPolygon
+}
+
+
+def create_figures(input_data: dict) -> list:
+    result = []
+    for f_info in input_data:
+        figure_type = f_info['type']
+        if figure_type in FIGURE_TYPES:
+            figure_class = FIGURE_TYPES[figure_type]
+            result.append(figure_class(**f_info))
+        else:
+            raise ValueError('Unsupported figure')
+    return result
+
+
+def draw_figures(figures):
+    for figure in figures:
+        t = turtle.Turtle()
+        t.speed('fast')
+        figure.draw(t)
+
+    turtle.exitonclick()
+
+
+if __name__ == "__main__": 
+    main()
